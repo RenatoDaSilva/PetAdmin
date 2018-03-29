02fac948571b92eb90e2c4d5fe8a3d689f3eba96
RenatoDaSilva <renato@asseinfo.com.br>
2018-03-29 19:29:40 -0300

    Gerando os Recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :sells
+    resources :suppliers
+    resources :campaigns
+    resources :discounts
+    resources :services
+    resources :products
+    resources :clients
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
 
   require 'sidekiq/web'
-
-  Rails.application.routes.draw do
-
-    namespace :admin do
-    end
-    # mount Fae below your admin namespec
-    mount Fae::Engine => '/admin'
-
-    mount Sidekiq::Web => '/sidekiq'
-  end
 end
