cafcb9e5d4bf3dfc451aba35163af5d6a6b21ad9
RenatoDaSilva <renato@asseinfo.com.br>
2018-03-30 00:38:54 -0300

    Customizando o Comportamento 2

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,15 +1,17 @@
 class Sell < ApplicationRecord
-include Fae::BaseModelConcern
-enum status: { finished: 0, canceled: 1 }
+  include Fae::BaseModelConcern
+  enum status: {finished: 0, canceled: 1}
 
-def fae_display_field
-  id
-end
+  validates :client, presence: true
 
-def self.for_fae_index
-  order(:id)
-end
+  def fae_display_field
+    id
+  end
 
-belongs_to :discount
-belongs_to :client
+  def self.for_fae_index
+    order(:id)
+  end
+
+  belongs_to :discount
+  belongs_to :client
 end
